993296d4d30076bf2362c6f250d96bad53b8a333
MrKitty <107802643+MrKittyOfc@users.noreply.github.com>
2023-04-11 19:44:06 +0200

    Create key.lua

---
+++ key.lua
@@ -0,0 +1,14 @@
+-- Library
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+-- Key System
+local KeySystem = true
+local KeySettings = {
+   Title = "Sirius Hub",
+   Subtitle = "Key System",
+   Note = "Join the discord (discord.gg/sirius)",
+   FileName = "SiriusKey",
+   SaveKey = false,
+   GrabKeyFromSite = false,
+   Key = "Hello"
+}
