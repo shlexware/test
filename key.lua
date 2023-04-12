0db1d87da6250fb92711c8b18529f25830cd90db
MrKitty <107802643+MrKittyOfc@users.noreply.github.com>
2023-04-12 21:20:06 +0200

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,16 +1,1 @@
--- Library
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
-
--- Key System
-local KeySystem = true
-local KeySettings = {
-   Title = "Sirius Hub",
-   Subtitle = "Key System",
-   Note = "Join the discord (discord.gg/sirius)",
-   FileName = "SiriusKey",
-   SaveKey = false,
-   GrabKeyFromSite = false,
-   Key = "Hello"
-}
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/MrKittyOfc/test/main/more.lua'))()
+LSUNI
